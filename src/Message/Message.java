7d99a753232f02515d6519c71a4fe1249ed39fc3
Alex Zelentsov <32770870+alexzele@users.noreply.github.com>
2018-02-13 19:02:14 +0200

    Revert "last ver."
    This reverts commit 7be5360e88c42bb74f6333edf94ac900a913301c, reversing
    changes made to 7d7991168f10910738b0db0fc2011785298a96ff.

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -12,15 +12,16 @@ public abstract class Message implements Serializable{ //abstract class because 
 
 	//fields
 	public enum Header {LOGIN,
-		LOG_OUT,
+		LOGOUT,
+		GET_ALL_TENANTS_PAYMENTS,
 		GET_PAYMENTS_BY_TENANT_ID,
-		GET_BUILDING_PAYMENTS_BY_APARTMENT,
 		SET_NEW_BUILDING,
+		GET_BUILDING_PAYMENTS_BY_MONTH,
 		UPDATE_TENANT_PAYMENT_BY_MONTH,
 		DELETE_TENANT_PAYMENT_BY_MONTH,
 		GET_BUILDING_MONTHLY_REVENUE,
 		GET_CONTRACTOR,
-		SET_CONTRACTOR};
+		INSERT_CONTRACTOR}
 	public enum Sender {TENANT,COMMITTEE}
 	private ArrayList<String> content;
 
